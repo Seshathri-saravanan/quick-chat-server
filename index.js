85d7872e348db70da3b249d353e005c3805eb598
seshathri S <seshathrisaravanan@gmail.com>
2021-08-02 14:48:25 +0530

    changed cors origin

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(cors(
 app.use(cookieParser("oisdfbkdufhejbfibufgvfuvsfu"));
 
 function addHeaders(req,res,next){
-  res.setHeader('Access-Control-Allow-Origin','http://localhost:3000',);
+  res.setHeader('Access-Control-Allow-Origin','https://quick-chat-2021.herokuapp.com',);
   res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
   res.setHeader('Access-Control-Allow-Credentials', true);
