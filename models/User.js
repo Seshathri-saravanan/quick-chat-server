8d6ee389f3a7d657e0ff3f5ca986ffbeeb1e10c0
Seshathri S <seshathris@Seshathris-MacBook-Air.local>
2022-09-24 10:18:47 +0530

    changed user schema

--- models/User.js
+++ models/User.js
@@ -8,6 +8,15 @@ var User = new Schema({
     unique: true,
     required: true,
   },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
   password: {
     type: String,
     required: true,
